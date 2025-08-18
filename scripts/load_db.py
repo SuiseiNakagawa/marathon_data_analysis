3a2f73c41a8969e44ed81f6aba46332b00f9452f
SuiseiNakagawa <suisei.nakagawa2456@gmail.com>
2025-08-18 12:40:09 -0700

    Stop tracking redundant GPX files; keep locally

--- scripts/load_db.py
+++ scripts/load_db.py
@@ -5,10 +5,8 @@ con = sqlite3.connect('data/marathon_data.db')
 cur = con.cursor()
 
 tcx_df = pd.read_csv('data/data_clean/tcx_clean.csv')
-gpx_df = pd.read_csv('data/data_clean/gpx_clean.csv')
 
 tcx_df.to_sql('tcx_data', con, if_exists='replace', index=False)
-gpx_df.to_sql('gpx_data', con, if_exists='replace', index=False)
 
 con.commit()
 con.close()
